1cb3ea44d9180a7a4ef31c44c710a3d47877eac9
Simona <s.c.dulgheru@gmail.com>
2019-12-02 18:12:47 +0000

    generate Git Pdf

---
+++ api.js
@@ -0,0 +1,30 @@
+const axios = require (`axios`);
+require(`dotenv`).config()
+const api = {
+    getUser(username){
+        return axios
+        .get(`https://api.github.com/users/${username}?client_id=${
+            process.env.CLIENT_ID
+          }&client_secret=${process.env.CLIENT_SECRET}`)
+        .catch (err=>{
+            console.log(err);
+            process.exit(1) // exit out the app
+        })
+
+    },
+    getTotalStars(username){
+        return axios
+        .get(`https://api.github.com/users/${username}/repos?client_id=${
+            process.env.CLIENT_ID
+          }&client_secret=${process.env.CLIENT_SECRET}&per_page=100`)
+        .then(res =>{
+            return res.data.reduce((acc,curr)=>{
+                acc += curr.stargazers_count
+                return acc
+            },0)                                                   //check api docs
+
+        })
+    }
+ }
+
+ module.exports = api;
