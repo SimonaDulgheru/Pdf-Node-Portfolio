e594cd04260e686ba034a98a1a5725c56bd8b51f
Simona <s.c.dulgheru@gmail.com>
2019-12-04 12:13:08 +0000

    Readme with Gif demo

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Pdf-Node-Portfolio
+
+Avoid revoking your Git API KEY on your public repositories. Install dotenv in your terminal 
+
+![Install dotenv](https://user-images.githubusercontent.com/48987979/70140116-e830d100-168b-11ea-8568-33498c360636.PNG)
+
+Add require to your api.js 
+
+![Require](https://user-images.githubusercontent.com/48987979/70140233-304ff380-168c-11ea-9fb0-83843cfe0057.PNG)
+
+Create a file .env 
+
+![env](https://user-images.githubusercontent.com/48987979/70140504-bf5d0b80-168c-11ea-82be-2cf1abe7bcdf.PNG)
+
+Place your API-KEY into the .env file. 
+
+Create another file called .gitignore
+![Ignore](https://user-images.githubusercontent.com/48987979/70140844-8bceb100-168d-11ea-8892-512fa58ca6ed.PNG)
+
+Git is a version control that keeps track of all of your files. .gitignore stops Git from tracking files. Read more on GitHub Developer Guide.
+
+
+Preview 
+
+![Pdf Generator  (0)](https://user-images.githubusercontent.com/48987979/70141214-75752500-168e-11ea-8a7e-6caaf064c9b5.gif)
+
+ Go to your command line and run node index.  A PDF will be generated
+
+
+
+
--- generateHTML.js
+++ generateHTML.js
@@ -1,10 +1,10 @@
 const colors = {
   green: {
-      wrapperBackground: "#E6E1C3",
-      headerBackground: "#C1C72C",
+      wrapperBackground: "rgb(70, 70, 69)",
+      headerBackground: "green",
       headerColor: "black",
-      photoBorderColor: "black",
-      links: "darkgreen"
+      photoBorderColor: "white",
+      links: "white"
   },
   blue: {
       wrapperBackground: "#5F64D3",
@@ -36,220 +36,239 @@ function generateHTML(data) {
   return `<!DOCTYPE html>
   <html lang="en">
   
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-      <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans|Josefin+Slab|Nanum+Myeongjo&display=swap" rel="stylesheet">
-      
-  
-      
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet">
+  <title>${data.response.data.name}</title>
       <style>
-              #topCard{
-              margin-right: 100px;
-              margin-left: 30px;
-              margin-top: 45px;
-              border-radius: 15px !important;
-              position: relative;
-              background-color: ${colors[data.color].headerBackground};
-              margin: auto;
-              color: ${colors[data.color].headerColor};
-              border:1px solid black;
-              }
-  
-              img{
-              display: block;
-              margin-left: auto;
-              margin-right: auto;
-              border-radius:50%;
-              border:2px solid ${colors[data.color].photoBorderColor};
-              max-width: 120px;
-              }
-  
-              h1{
-                  text-align: center;
-                  font-family: 'Josefin Slab', serif;
-              }
-  
-              h2{
-                  text-align: center;
-                  font-family: 'Josefin Slab', serif;
-              }
-              h3{
-                  font-size:15px;
-                  text-align: center;
-                  font-family: 'Alegreya Sans', sans-serif;
-              }
-  
-              h4{
-                  font-size: 20px;
-                  font-family: 'Nanum Myeongjo', serif;
-              }
-              p{
-                  font-family: 'Alegreya Sans', sans-serif;
-              }
-  
-              body{
-                  background-color:${colors[data.color].wrapperBackground};
-              }
-                 
-              a{
-                  color: ${colors[data.color].photoBorderColor};
-              }
-              .col-4{
-                  text-align: center;
-                  color: lightgray;
-              }
-              .blog{
-                  text-align: center;
-              }
-              
-              #bio{
-                  background-color:lightgray;
-                  color:black;
-                  border:1px solid black;
-              }
-              #contact{
-                  text-align: center;
-                  padding:10px;
-                  font-family: 'Nanum Myeongjo', serif;
-              }
-  
-              .infoCards{
-                  margin: auto 0;
-                  text-align: center;
-                  background:white;
-                  border: 1px solid black;
-              }
-              .left{
-                  padding-right:0;
-              }
+      @page {
+        margin: 0;
+      }
+     *,
+     *::after,
+     *::before {
+     box-sizing: border-box;
+     }
+     html, body {
+     padding: 0;
+     margin: 0;
+     }
+     html, body, .wrapper {
+     height: 100%;
+     }
+     .wrapper {
+     background-color: ${colors[data.color].wrapperBackground};
+     padding-top: 100px;
+     }
+     body {
+     background-color: white;
+     -webkit-print-color-adjust: exact !important;
+     font-family: 'Varela Round', sans-serif;
+     }
+     main {
+     background-color: #E9EDEE;
+     height: auto;
+     padding-top: 30px;
+     }
+     h1, h2, h3, h4, h5, h6 {
+    font-family: 'Varela Round', sans-serif;
+     margin: 0 auto;
+     }
+     h1 {
+     font-size: 3em;
+     }
+     h2 {
+     font-size: 2.5em;
+     }
+     h3 {
+     font-size: 2em;
+     padding: 30px;
+     margin:20px;
+     }
+     h4 {
+     font-size: 2em;
+     
+     }
+     h5 {
+     font-size: 1.3em;
+     }
+     h6 {
+     font-size: 1.2em;
+     }
+     .photo-header {
+     position: relative;
+     margin: 0 auto;
+     margin-bottom: -50px;
+     display: flex;
+     justify-content: center;
+     flex-wrap: wrap;
+     background-color: ${colors[data.color].headerBackground};
+     color: ${colors[data.color].headerColor};
+     padding: 10px;
+     width: 95%;
+     border-radius: 6px;
+     }
+     .photo-header img {
+     width: 300px;
+     height: 300px;
+     border-radius: 50%;
+     object-fit: cover;
+     margin-top: -75px;
+     margin-left:40%;
+     margin-right:50%;
+     border: 6px solid ${colors[data.color].photoBorderColor};
+     box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
+     }
+     .photo-header h1, .photo-header h2, .photo-header h4 {
+     width: 100%;
+     text-align: center;
+     }
+     .photo-header h1 {
+     margin-top: 10px;
+     }
+     .links-nav {
+     width: 100%;
+     text-align: center;
+     padding: 20px 0;
+     font-size: 1.8em;
+     }
+     .nav-link {
+     display: inline-block;
+     margin: 5px 10px;
+     }
 
-      </style>
-      <title>${data.response.name}</title>
-  </head>
-  
+     
+     .workExp-date {
+     font-style: italic;
+     font-size: .7em;
+     text-align: right;
+     margin-top: 10px;
+     }
+     .container {
+     padding: 50px;
+     padding-left: 100px;
+     padding-right: 100px;
+     }
+
+     .row-1 {
+        display: inline-block;
+        margin:20px;
+       
+       
+     }
+     .row-2 {
+        display: inline-block;
+        margin-top:20px;
+      }
+
+     .card {
+       padding: 15px;
+       border-radius: 6px;
+       background-color: ${colors[data.color].headerBackground};
+       color: ${colors[data.color].headerColor};
+       width: 550px;
+       height: 300px;
+     }
+     .card-repos{
+         margin-right:50px;
+         margin-left:150px;
+         margin-bottom: 100px;
+     }
+     .card-stars{
+        margin-right:50px;
+        margin-left:150px;
+        margin-top:20px;
+
+     }
+     .card-followers{
+        margin-right:50px;
+        margin-left:100px;
+        margin-bottom: 100px;
+
+     }
+     .card-following{
+        margin-right:50px;
+        margin-left:110px;
+        margin-top:20px;
+
+     }
+     
+     .col {
+     flex: 1;
+     text-align: center;
+     }
+
+     a, a:hover {
+     text-decoration: none;
+     color: inherit;
+     font-weight: bold;
+     }
+
+     @media print { 
+      body { 
+        zoom: .75; 
+      } 
+     }
+  </style>
+</head>
 <body>
-<header>
-<img id="front-page-image" src=${data.response.avatar_url}/>
-</header>
-<div class = "container">
-  <div id="topCard">
-      <div class = "row"> 
-          <div class = "col-12">
-              <h1>Hi!</h1>
-          </div>
-      </div>
-      <div class = "row"> 
-          <div class = "col-12">
-              <h2>My name is ${data.response.name}</h2>
-          </div>
-      </div>
-      <div class = "row"> 
-          <div class = "col-12">
-              <h2>Currently @ Coding Bootcamp</h2>
-          </div>
-      </div>
-  
-  <div class = "links">
-      <div class = "row">
-          <div class = "col-4">    
-              <nav>
-              ${data.response.location ? `<a class="nav-link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/
-              ${data.response.location}"><i class="fas fa-location-arrow"></i> ${data.response.location}</a>` : " "}
-              </nav>
-          </div>
-          <div class = "col-4">  
-              <a class="github" style="text-align:center" href="${data.response.html_url}"> <i class="fab fa-github-alt"></i> GitHub</a>
-          </div>
-          <div class = "col-4">  
-              <a class="blog" style="text-align:center" href="${data.response.blog}">Blog</a>
-          </div>
+  <div class="wrapper">
+      <div class="container">
+            <header class="photo-header">
+              <img  src=${data.response.data.avatar_url} />
+                  <h1>Hi!</h1>
+                  <h2>My name is Simona Dulgheru</h2>
+                  <h4>Currently @ Coding Bootcamp</h4>
+                    <ul class="links-nav">
+                        <li class="nav-link"><a href="http://google.com/maps/place/${data.response.data.location}/"target="blank">${data.response.data.location}</a></li>
+                        <li class="nav-link"><a href="${data.response.data.html_url}">Github</a></li>
+                        <li class="nav-link"><a href="${data.response.data.blog}">Blog</a></li>
+                    </ul>
+            </header>
+          <main>
+              <div class="container">
+                  <div class="row">
+                      <div class="col">
+                          <h3>${data.response.data.bio}</h3>
+                      </div>
+                  </div>
+                  <div class="row-1">
+                    <div class="col">
+                      <div class="card card-repos">
+                        <h3>Public Repositories</h3>
+                        <h4>${data.response.data.public_repos}</h4>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="card card-stars">
+                        <h3>GitHub Stars</h3>
+                        <h4>${data.response.stars ? `${data.response.stars}` : "0"}</h4>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row-2">
+                    <div class="col">
+                      <div class=" card card-followers">
+                        <h3>Followers</h3>
+                        <h4>${data.response.data.followers}</h4>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class=" card card-following">
+                        <h3>Following</h3>
+                        <h4>${data.response.data.following}</h4>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </main>
       </div>
   </div>
-  
-  
-   <!-- git hub bio  -->
-  <div id="bio">
-      <div class = "row"> 
-          <div class = "col-12">
-              <h3>${data.response.bio ? `${data.response.bio}` : " "} </h3>
-          </div>
-      </div>
-  </div>
-  
-      <div class="infoCards">
-              
-          <!-- pubic repo -->
-          <div class = "row">
-              <div class = "col-6">
-                  <div class = "row"> 
-                      <div class = "col-12">
-                          <h4>Public Repositories</h4>
-                      </div>
-                  </div>
-                  <div class = "row"> 
-                      <div class = "col-12">
-                          <p>${data.response.public_repos}</p>
-                      </div>
-                  </div>
-              </div>
-                                  
-              <!--  followers -->
-              <div class = "col-6">
-                  <div class = "row"> 
-                      <div class = "col-12">
-                          <h4>Followers</h4>
-                      </div>
-                  </div>
-                  <div class = "row"> 
-                      <div class = "col-12">
-                          <p>${data.response.followers}</p>
-                      </div>
-                  </div>
-              </div>
-          </div>         
-              <!-- stars -->
-          <div class = "row">
-                  <div class = "col-6">
-                      <div class = "row"> 
-                          <div class = "col-12">
-                              <h4>GitHub Stars</h4>
-                          </div>
-                      </div>
-                      <div class = "row"> 
-                          <div class = "col-12">
-                              <p> ${data.response.stars ? `${data.response.stars}` : "None"}</p>
-                          </div>
-                      </div>
-                  </div>
-                                  
-               <!-- following -->
-              <div class = "col-6">
-                  <div class = "row"> 
-                      <div class = "col-12">
-                          <h4>Following</h4>
-                      </div>
-                  </div>
-                  <div class = "row"> 
-                      <div class = "col-12">
-                          <p>${data.response.following}</p>
-                      </div>
-                  </div>
-              </div>
-          </div>     
-
-      </div>
-  
-
-  </div>            
-  
-  
-  </body>
-  
-  </html>`;
+</body>
+</html>`;
 }
 
 module.exports = generateHTML;
